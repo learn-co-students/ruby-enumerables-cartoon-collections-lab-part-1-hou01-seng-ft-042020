cabe723b7c98f7f2bd39e59e1eae2e593d5f8935
Lauren Birts <birtslauren@gmail.com>
2020-03-30 23:28:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,13 @@
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
-  #
+  array.count { |dwarf|
+    puts "Hello #{dwarf}!"
+  }
   # Print a custom greeting for each element
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
-  #
+   array.each.with_index(1) { |dwarf, index| puts "#{index}. #{dwarf}" }
   # Print a numbered list of each element
 end
